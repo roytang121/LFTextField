91722579d75e2e1b47bdbda6be40f22e85db9fc7
ustbenchan <ustbenchan@gmail.com>
2016-06-20 17:58:19 +0800

    Make the auto-check function controllable

--- LFTextField/Classes/LFTextField.swift
+++ LFTextField/Classes/LFTextField.swift
@@ -35,33 +35,39 @@ func getResourcesBundle() -> NSBundle? {
 @IBDesignable
 public class LFTextField: UIView, PredicateInspectorDelegate {
 
-  
+
   private var overlay: LFTextFieldOverlay! {
     didSet {
       self.inputTextField = self.overlay?.textField
     }
   }
-  
+
   private var isIB: Bool = false
-  
+
   private var inspector: PredicateInspector!
-  
+
   static let kFieldTypeNormal = "normal"
   static let kFieldTypeEmail = "email"
 //  static let kFieldTypePassword = "password"
-  
+
   // MARK: public properties
   public weak var inputTextField: UITextField!
-  
+
   public weak var delegate: LFTextFieldDelegate? {
     didSet {
       self.inputTextField?.delegate = delegate as? UITextFieldDelegate
     }
   }
-  
+
+  public var enableAutoCheck: Bool = true {
+    didSet {
+      self.inspector?.shouldAutoCheckTextView = enableAutoCheck
+    }
+  }
+
   var state: LFTextFieldOverlay.State = .Normal {
     didSet {
-      
+
       if self.state == .OK {
         self.overlay?.state = self.showSuccessState ? .OK : .Normal;
       } else {
@@ -69,43 +75,43 @@ public class LFTextField: UIView, PredicateInspectorDelegate {
       }
     }
   }
-  
+
   @IBInspectable var placeholder: String! {
     didSet {
       overlay?.textField.placeholder = self.placeholder
     }
   }
-  
+
   @IBInspectable var title: String! {
     didSet {
       overlay?.titleLabel?.text = title
     }
   }
-  
+
   @IBInspectable var titleIcon: String? {
     didSet {
       overlay?.titleIconAssetName = titleIcon
     }
   }
-  
+
   @IBInspectable var alertColor: UIColor! {
     didSet {
       overlay?.colorAlert = alertColor
     }
   }
-  
+
   @IBInspectable var fieldType: String? {
     didSet {
-      
+
       if customRegex != nil {
         print("Field Type could not be set together with Custom Regex")
         return
       }
-      
+
       if fieldType == nil {
         return
       }
-      
+
       switch fieldType! {
       case LFTextField.kFieldTypeEmail:
         self.inspector?.pattern = PredicateInspector.kPatternEmail
@@ -115,118 +121,118 @@ public class LFTextField: UIView, PredicateInspectorDelegate {
       }
     }
   }
-  
+
   @IBInspectable var customRegex: String? {
     didSet {
-      
+
       if fieldType != nil {
         print("Field Type could not be set together with Custom Regex")
         return
       }
-      
+
       if let regex = self.customRegex?.stringByReplacingOccurrencesOfString("\\\\", withString: "\\") {
         self.inspector?.pattern = Pattern(name: "custom", regex: regex)
       }
     }
   }
-  
+
   @IBInspectable var displayTitleLabel: Bool = true {
     didSet {
       overlay?.displayTitleLabel = self.displayTitleLabel
     }
   }
-  
+
   @IBInspectable var showSuccessState: Bool = true {
     didSet {
-      
+
     }
   }
-  
+
   public var style: LFBorderStyle = .Rounded {
     didSet {
       self.overlay.style = self.style
     }
   }
-  
+
   // MARK: Popup Message Properties
-  
+
   var popup: UILabel!
-  
+
   var popUpFontSize: CGFloat = 11.0
-  
+
   var popUpleftPadding: CGFloat = 8.0 {
     didSet {
       self.popup?.frame.origin = CGPointMake(popUpleftPadding, self.overlay.bounds.height + popUpTopPadding)
     }
   }
-  
+
   var popUpTopPadding: CGFloat = 3.0
-  
+
   @IBInspectable public var popupTitle: String! {
     didSet {
       self.popup?.text = self.popupTitle
     }
   }
-  
+
   override init(frame: CGRect) {
     super.init(frame: frame)
-    
+
     self.setup()
-    
+
   }
-  
+
   required public init?(coder aDecoder: NSCoder) {
     super.init(coder: aDecoder)
-    
+
     self.setup()
   }
-  
+
   func setup() {
     self.overlay = NSBundle(forClass: self.dynamicType).loadNibNamed("LFTextFieldOverlay", owner: self, options: nil)[0] as? LFTextFieldOverlay
-    
+
     self.addSubview(self.overlay)
-    
+
     self.overlay.textField.delegate = self
     self.overlay.textField.addTarget(self, action: Selector("textFieldValueDidChange:"), forControlEvents: .EditingChanged)
-    
+
     self.inspector = PredicateInspector(textField: self.overlay.textField)
     self.inspector.delegate = self
-    
+
     self.inspector.pattern = PredicateInspector.kPatternEmail
   }
-  
+
   override public func resignFirstResponder() -> Bool {
     return self.inputTextField?.resignFirstResponder() ?? super.resignFirstResponder()
   }
-  
+
   override public func becomeFirstResponder() -> Bool {
     return self.inputTextField?.becomeFirstResponder() ?? super.becomeFirstResponder()
   }
-  
+
   override public func layoutSubviews() {
     super.layoutSubviews()
-    
+
     self.overlay?.frame = CGRectMake(0, 0, bounds.width, bounds.height - popUpFontSize - popUpTopPadding)
-    
+
     addPopupMessage(self.popupTitle)
   }
-  
+
   // MARK: @IBDesignable
   override public func prepareForInterfaceBuilder() {
     super.prepareForInterfaceBuilder()
-    
+
     self.isIB = true
-    
+
     addPopupMessage(self.popupTitle)
-    
+
 //    self.layer.borderColor = UIColor(rgba: "#ddd").CGColor
-//    
+//
 //    self.layer.borderWidth = 0.8
-//    
+//
 //    self.layer.cornerRadius = 4.0
 //    self.hidePopup()
   }
-  
+
   private func addPopupMessage(message: String!) {
     if self.popup == nil || isIB {
       let popup = UILabel()
@@ -236,79 +242,79 @@ public class LFTextField: UIView, PredicateInspectorDelegate {
       popup.sizeToFit()
       popup.frame.origin = CGPointMake(popUpleftPadding, self.overlay.bounds.height + popUpTopPadding)
       popup.alpha = 0.6
-    
+
       self.addSubview(popup)
-      
+
       self.popup = popup
     }
-    
+
     if !isIB {
       self.hidePopupAnimated(false)
     }
   }
-  
+
   public func showPopupWithTitle(message _message: String?) {
     if let message = _message {
       self.popup.text = message
       self.popup.sizeToFit()
     }
-    
+
     if let popup = self.popup {
       popup.frame.offsetInPlace(dx: 0, dy: -popup.bounds.height)
-      
+
       popup.alpha = 0.0
-      
+
       UIView.animateWithDuration(0.85, delay: 0.0, usingSpringWithDamping: 0.3, initialSpringVelocity: 0.5, options: UIViewAnimationOptions.CurveEaseInOut, animations: { () -> Void in
-        
+
         popup.frame.offsetInPlace(dx: 0, dy: popup.bounds.height)
         popup.alpha = 1.0
-        
+
         }, completion: nil)
     }
   }
-  
+
   func hidePopupAnimated(animated: Bool = true) {
     if !animated {
-      
+
         self.popup?.alpha = 0.0
     } else {
       UIView.animateWithDuration(0.8, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 0, options: UIViewAnimationOptions.CurveEaseOut, animations: { () -> Void in
-        
+
         // dismiss animated
         self.popup?.alpha = 0.0
-        
+
         }, completion: nil)
     }
   }
-  
+
   public func isValid() -> Bool {
     self.inspector.inspect(self.inputTextField)
     return self.inspector.lastMatchResult ?? false
   }
-  
-  
+
+
   // MARK: Predicate Inspector Protocol
   func predicateInspectorMatchSuccess(inspector: PredicateInspector, pattern: Pattern) {
     self.state = .OK
-    
+
     self.hidePopupAnimated(true)
   }
-  
+
   func predicateInspectorMatchFail(inspector: PredicateInspector, pattern: Pattern) {
     self.state = .Alert
     self.showPopupWithTitle(message: self.popupTitle)
   }
-  
+
   deinit {
     self.overlay = nil
     self.inspector = nil
     self.inputTextField = nil
   }
-  
+
 }
 
 extension LFTextField: UITextFieldDelegate {
-  
+
   // textfield target selector
   func textFieldValueDidChange(sender: AnyObject?) {
     self.delegate?.textFieldValueDidChange(sender)
